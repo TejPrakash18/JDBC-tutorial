fb20593a7c0a43eb097177184e7005673e43c474
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-05-09 22:11:41 +0530

    SwingApp

---
+++ src/Projects/Demo/AWTApp.java
@@ -0,0 +1,12 @@
+package Projects.Demo;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class AWTApp {
+    public static void main(String[] args) {
+        Frame frame = new Frame("Awt window");
+        frame.setSize(500,400);
+        frame.setVisible(true);
+    }
+}
---
+++ src/Projects/Demo/SwingApp2.java
@@ -0,0 +1,12 @@
+package Projects.Demo;
+
+import javax.swing.*;
+
+public class SwingApp2 {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Swing App");
+        jFrame.setSize(500,500);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
+    }
+}
