4b0fb711354c0ccd3b0bd9025ef69891395deaeb
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-05-21 21:56:19 +0530

    loginGUI

--- src/GUI/LoginPage.java
+++ src/GUI/LoginPage.java
@@ -4,141 +4,143 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-class Register{
-    public Register(){
-        JFrame f=new JFrame("Registration form");
+class Register {
+    public Register() {
+        JFrame registerFrame = new JFrame("Home Page");
 
-        JLabel heading=new JLabel("Print data on console....");
-        heading.setBounds(70,20,180,40);
+        JLabel heading = new JLabel("Print data on console...");
+        heading.setBounds(70, 20, 180, 40);
 
-        JLabel firstName=new JLabel("First Name");
-        firstName.setBounds(20,50,80,20);
+        JLabel firstNameLabel = new JLabel("First Name");
+        firstNameLabel.setBounds(20, 50, 80, 20);
 
-        JLabel lastName = new JLabel("Last name");
-        lastName.setBounds(20,75,80,30);
+        JLabel lastNameLabel = new JLabel("Last Name");
+        lastNameLabel.setBounds(20, 80, 80, 20);
 
-        JLabel dob=new JLabel("Date-of-Birth");
-        dob.setBounds(20,105,80,30);
+        JLabel dobLabel = new JLabel("Date of Birth");
+        dobLabel.setBounds(20, 110, 80, 20);
 
-        JTextField firstNameTf=new JTextField();
-        firstNameTf.setBounds(120,50,100,20);
+        JTextField firstNameField = new JTextField();
+        firstNameField.setBounds(120, 50, 100, 20);
 
-        JTextField lastNameTf=new JTextField();
-        lastNameTf.setBounds(120, 80, 100, 20);
+        JTextField lastNameField = new JTextField();
+        lastNameField.setBounds(120, 80, 100, 20);
 
-        JTextField  dobtf=new JTextField();
-        dobtf.setBounds(120, 110, 100, 20);
+        JTextField dobField = new JTextField();
+        dobField.setBounds(120, 110, 100, 20);
 
-        JLabel email = new JLabel("E-mail");
-        email.setBounds(20,140,100,20);
+        JLabel emailLabel = new JLabel("E-mail");
+        emailLabel.setBounds(20, 140, 80, 20);
 
-        JTextField emailTF=new JTextField();
-        emailTF.setBounds(120,140,100,20);
+        JTextField emailField = new JTextField();
+        emailField.setBounds(120, 140, 100, 20);
 
-        JButton sbmt = new JButton("Submit");
-        sbmt.setBounds(20, 180, 100, 30);
-
-        sbmt.addActionListener(new ActionListener() {
+        JButton submitButton = new JButton("Submit");
+        submitButton.setBounds(20, 180, 100, 30);
+        submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String name = firstNameTf.getText();
-                String lName = lastNameTf.getText();
-                String dob = dobtf.getText();
-                String email=emailTF.getText();
-                System.out.println(name);
-                System.out.println(lName);
+                String firstName = firstNameField.getText();
+                String lastName = lastNameField.getText();
+                String dob = dobField.getText();
+                String email = emailField.getText();
+                System.out.println(firstName);
+                System.out.println(lastName);
                 System.out.println(dob);
                 System.out.println(email);
             }
         });
 
-        JButton reset = new JButton("Reset");
-        reset.setBounds(140,180,100,30);
-        reset.addActionListener(new ActionListener() {
+        JButton resetButton = new JButton("Reset");
+        resetButton.setBounds(140, 180, 100, 30);
+        resetButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                firstNameTf.setText("");
-                lastNameTf.setText("");
-                dobtf.setText("");
-                emailTF.setText("");
+                firstNameField.setText("");
+                lastNameField.setText("");
+                dobField.setText("");
+                emailField.setText("");
             }
         });
 
-        f.add(heading);
-        f.add(firstName);
-        f.add(lastName);
-        f.add(dob);
-        f.add(firstNameTf);
-        f.add(lastNameTf);
-        f.add(dobtf);
-        f.add(sbmt);
-        f.add(reset);
-        f.add(email);
-        f.add(emailTF);
+        registerFrame.add(heading);
+        registerFrame.add(firstNameLabel);
+        registerFrame.add(lastNameLabel);
+        registerFrame.add(dobLabel);
+        registerFrame.add(firstNameField);
+        registerFrame.add(lastNameField);
+        registerFrame.add(dobField);
+        registerFrame.add(emailLabel);
+        registerFrame.add(emailField);
+        registerFrame.add(submitButton);
+        registerFrame.add(resetButton);
 
-        f.setSize(500,500);
-        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        f.setLayout(null);
-        f.setVisible(true);
+        registerFrame.setSize(300, 300);
+        registerFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        registerFrame.setLayout(null);
+        registerFrame.setVisible(true);
     }
 }
 
 public class LoginPage {
-    public LoginPage(){
-        JFrame f=new JFrame("Login page");
-        JLabel heading=new JLabel("login");
-        heading.setBounds(70,20,180,30);
+    public LoginPage() {
+        JFrame loginFrame = new JFrame("Login Page");
 
-        JLabel Name = new JLabel("UserName");
-        Name.setBounds(20,50,80,20);
+        JLabel heading = new JLabel("Login");
+        heading.setBounds(70, 20, 180, 30);
 
-        JTextField nameTF=new JTextField();
-        nameTF.setBounds(120,50,100,20);
+        JLabel usernameLabel = new JLabel("Username");
+        usernameLabel.setBounds(20, 50, 80, 20);
 
-        JLabel Pass =new JLabel("Password");
-        Pass.setBounds(20,75,80,30);
+        JTextField usernameField = new JTextField();
+        usernameField.setBounds(120, 50, 100, 20);
 
-        JTextField passTF=new JTextField();
-        passTF.setBounds(120,80,100,20);
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(20, 80, 80, 20);
 
-        JButton sbmt = new JButton("Login");
-        sbmt.setBounds(20, 120, 100, 30);
-        sbmt.addActionListener(new ActionListener() {
+        JTextField passwordField = new JTextField();
+        passwordField.setBounds(120, 80, 100, 20);
+
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(20, 120, 100, 30);
+        loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String pass = passTF.getText();
-                if (pass.equals("gungun")) {
+                String password = passwordField.getText();
+                if (password.equals("gungun")) {
                     new Register();
+                    loginFrame.dispose();
                     System.out.println("Welcome");
+                } else {
+                    System.out.println("Failed");
                 }
-
-                else System.out.println("Failed");
             }
         });
 
-
-        JButton reset = new JButton("Reset");
-        reset.setBounds(140,120,100,30);
-
-        reset.addActionListener(e -> {
-            nameTF.setText("");
-            passTF.setText("");
+        JButton resetButton = new JButton("Reset");
+        resetButton.setBounds(140, 120, 100, 30);
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                usernameField.setText("");
+                passwordField.setText("");
+            }
         });
 
+        loginFrame.add(heading);
+        loginFrame.add(usernameLabel);
+        loginFrame.add(usernameField);
+        loginFrame.add(passwordLabel);
+        loginFrame.add(passwordField);
+        loginFrame.add(loginButton);
+        loginFrame.add(resetButton);
 
-        f.add(heading);
-        f.add(Name);
-        f.add(nameTF);
-        f.add(Pass);
-        f.add(passTF);
-        f.add(sbmt);
-        f.add(reset);
-
-        f.setSize(500,500);
-        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        f.setLayout(null);
-        f.setVisible(true);
+        loginFrame.setSize(300, 300);
+        loginFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        loginFrame.setLayout(null);
+        loginFrame.setVisible(true);
     }
+
     public static void main(String[] args) {
         new LoginPage();
     }
