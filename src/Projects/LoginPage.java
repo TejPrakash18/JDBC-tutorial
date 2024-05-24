1f3cf297bb89cb80fdfdb3ad3d190e4cf0af469e
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-05-24 22:01:30 +0530

    todo

---
+++ src/Projects/LoginPage.java
@@ -0,0 +1,88 @@
+package Projects;
+
+//import GUI.ProfilePage;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+class LoginPage {
+    public LoginPage() {
+        JFrame loginFrame = new JFrame("Login Page");
+        ImageIcon appIcon = new ImageIcon("../thenotchers.png");
+        loginFrame.setIconImage(appIcon.getImage());
+
+        JLabel heading = new JLabel("Login");
+        heading.setBounds(70, 20, 180, 30);
+
+        JLabel usernameLabel = new JLabel("E-mail");
+        usernameLabel.setBounds(20, 50, 80, 20);
+
+        JTextField usernameField = new JTextField();
+        usernameField.setBounds(120, 50, 100, 20);
+
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(20, 80, 80, 20);
+
+        JPasswordField passwordField = new JPasswordField();
+        passwordField.setBounds(120, 80, 100, 20);
+
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(20, 120, 100, 30);
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String email = usernameField.getText();
+                String password = new String(passwordField.getPassword());
+
+                try (Connection connection = DatabaseConnection.getInstance().getConnection()) {
+                    String query = "SELECT * FROM users WHERE email = ? AND password = ?";
+                    PreparedStatement preparedStatement = connection.prepareStatement(query);
+                    preparedStatement.setString(1, email);
+                    preparedStatement.setString(2, password);
+                    ResultSet resultSet = preparedStatement.executeQuery();
+
+                    if (resultSet.next()) {
+                        System.out.println("Welcome");
+                        loginFrame.dispose();
+                    } else {
+                        System.out.println("Invalid credentials");
+                    }
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        JButton resetButton = new JButton("Reset");
+        resetButton.setBounds(140, 120, 100, 30);
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                usernameField.setText("");
+                passwordField.setText("");
+            }
+        });
+
+        loginFrame.add(heading);
+        loginFrame.add(usernameLabel);
+        loginFrame.add(usernameField);
+        loginFrame.add(passwordLabel);
+        loginFrame.add(passwordField);
+        loginFrame.add(loginButton);
+        loginFrame.add(resetButton);
+
+        loginFrame.setSize(500, 400);
+        loginFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        loginFrame.setLayout(null);
+        loginFrame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Register();
+    }
+}
---
+++ src/Projects/Register.java
@@ -0,0 +1,111 @@
+package Projects;
+
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+class Register {
+    public Register() {
+        JFrame registerFrame = new JFrame("Register Page");
+        ImageIcon appIcon = new ImageIcon("thenotchers.png");
+        registerFrame.setIconImage(appIcon.getImage());
+
+        JLabel heading = new JLabel("Register");
+        heading.setBounds(70, 20, 180, 40);
+
+        JLabel firstNameLabel = new JLabel("First Name");
+        firstNameLabel.setBounds(20, 50, 80, 20);
+
+        JLabel lastNameLabel = new JLabel("Last Name");
+        lastNameLabel.setBounds(20, 80, 80, 20);
+
+        JLabel dobLabel = new JLabel("Date of Birth");
+        dobLabel.setBounds(20, 110, 80, 20);
+
+        JTextField firstNameField = new JTextField();
+        firstNameField.setBounds(120, 50, 100, 20);
+
+        JTextField lastNameField = new JTextField();
+        lastNameField.setBounds(120, 80, 100, 20);
+
+        JTextField dobField = new JTextField();
+        dobField.setBounds(120, 110, 100, 20);
+
+        JLabel emailLabel = new JLabel("E-mail");
+        emailLabel.setBounds(20, 140, 80, 20);
+
+        JTextField emailField = new JTextField();
+        emailField.setBounds(120, 140, 100, 20);
+
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(20, 170, 80, 20);
+
+        JPasswordField passwordField = new JPasswordField();
+        passwordField.setBounds(120, 170, 100, 20);
+
+        JButton submitButton = new JButton("Submit");
+        submitButton.setBounds(20, 210, 100, 30);
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String firstName = firstNameField.getText();
+                String lastName = lastNameField.getText();
+                String dob = dobField.getText();
+                String email = emailField.getText();
+                String password = new String(passwordField.getPassword());
+
+                try (Connection connection = DatabaseConnection.getInstance().getConnection()) {
+                    String query = "INSERT INTO users (first_name, last_name, dob, email, password) VALUES (?, ?, ?, ?, ?)";
+                    PreparedStatement preparedStatement = connection.prepareStatement(query);
+                    preparedStatement.setString(1, firstName);
+                    preparedStatement.setString(2, lastName);
+                    preparedStatement.setString(3, dob);
+                    preparedStatement.setString(4, email);
+                    preparedStatement.setString(5, password);
+                    preparedStatement.executeUpdate();
+                    System.out.println("User registered successfully");
+//                    new LoginPage();
+                    registerFrame.dispose();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        JButton resetButton = new JButton("Reset");
+        resetButton.setBounds(140, 210, 100, 30);
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                firstNameField.setText("");
+                lastNameField.setText("");
+                dobField.setText("");
+                emailField.setText("");
+                passwordField.setText("");
+            }
+        });
+
+        registerFrame.add(heading);
+        registerFrame.add(firstNameLabel);
+        registerFrame.add(lastNameLabel);
+        registerFrame.add(dobLabel);
+        registerFrame.add(firstNameField);
+        registerFrame.add(lastNameField);
+        registerFrame.add(dobField);
+        registerFrame.add(emailLabel);
+        registerFrame.add(emailField);
+        registerFrame.add(passwordLabel);
+        registerFrame.add(passwordField);
+        registerFrame.add(submitButton);
+        registerFrame.add(resetButton);
+
+        registerFrame.setSize(500, 400);
+        registerFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        registerFrame.setLayout(null);
+        registerFrame.setVisible(true);
+    }
+}
